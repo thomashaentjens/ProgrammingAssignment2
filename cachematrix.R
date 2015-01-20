b706ecba7b39b0389ff5b390437ef19fc777f135
thomashaentjens <haentjens.thomas@gmail.com>
2015-01-20 08:12:04 +0100

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those two function aim to return the invert of a square invertible matrix 
+## with a low usage of memory by putting variables in cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix is use to cache setters and getters in order to manipulate 
+## the square invertible matrix passed as agurment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL  ## Create an empty Variable
+  Set <- function(x1) { ## Setter for the cached matrix
+    x <<- x1 
+    i <<- NULL
+  }
+  Get <- function() x ## Getter for the cached matrix
+  SetInvert <- function(invert) i <<- invert ## Setter for a cached solved matrix
+  GetInvert <- function() i ## Getter for the cached solved matrix
+  ## Create a list of items with Getters and Setters
+  list(Set = Set,Get = Get,SetInvert = SetInvert,GetInvert = GetInvert)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve return the invert of a square invertible matrix cached with
+## the function makeCacheMatrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Get the cached invert of the matrix and test if it is NULL 
+  so <- x$GetInvert()
+  if(!is.null(so)){
+    ## advise the user that the invert has been retrive from the cache
+    message("getting data from cache")
+    ## retrun the cached invert
+    return(so)
+  }
+  ## Get the matrix into a local variable
+  matrix <- x$Get()
+  ## Compute the invert of the matrix with the solve function
+  solve <- solve(matrix)
+  ## Cache the invert in order to retrive it with low memory usage.
+  x$SetInvert(solve)
+  ## Return the invert
+  solve
 }
